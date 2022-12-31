a0fe5fb3cd5cc35c58c5d5c0aae874c9a0b89d6e
duypro432003 <vanduyluu432003@gmail.com>
2022-12-31 08:52:57 +0700

    landing page

---
+++ day7/script.js
@@ -0,0 +1,12 @@
+//alert('hello world!');
+// var myName = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+// } ;
+// alert(myName.firstName);
+
+// let a=1
+// a = a + 'a' + 1
+// // alert(typeof a)
+// alert("hello world!"-"hello")
+console.log("hello world!")
