3a6af2fd381b3f039879cdc796b06e187246556f
duypro432003 <vanduyluu432003@gmail.com>
2023-02-19 21:06:30 +0700

    add to do list web

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -1,9 +1,20 @@
 let displayAddingForm = document.getElementById("adding-form");
+let displayEditForm = document.getElementById("editing-form");
 let getAddBtn = document.getElementById("add-btn");
-let getCloseBtn = document.getElementById("close-Btn");
+let getCloseAddBtn = document.getElementById("close-Btn");
+let getCloseEditBtn=document.getElementById("close-edit-Btn");
+let getEditBtn = document.getElementsByClassName("editBtn");
 getAddBtn.addEventListener("click", function () {
     displayAddingForm.style.display = "block";
 });
-getCloseBtn.addEventListener("click", function () {
+getCloseAddBtn.addEventListener("click", function () {
     displayAddingForm.style.display = "none";
 });
+getCloseEditBtn.addEventListener("click", function () {
+    displayEditForm.style.display = "none";
+});
+for (let i = 0; i < getEditBtn.length; i++) {
+    getEditBtn[i].addEventListener("click", function () {
+        displayEditForm.style.display = "block";
+    });
+}
